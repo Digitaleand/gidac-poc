584eae6e9b3ddd2fdec925d6614d708eb55973ee
ollie314 <mehdi.lefebvre@gmail.com>
2022-11-20 09:32:54 +0100

    wip(init): add existing path policy
    Signed-off-by: ollie314 <mehdi.lefebvre@gmail.com>

---
+++ gidac-domain/gidac-domain-core/src/test/java/ch/vd/gidac/domain/core/policies/ExistingPathPolicyTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright(c) 2022 mehdi.lefebvre@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package ch.vd.gidac.domain.core.policies;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ExistingPathPolicyTest {
+
+  private final ExistingPathPolicy policy = new ExistingPathPolicy();
+
+  @Test
+  void validPolicy () {
+    // GIVEN
+    final var path = Paths.get( FileUtils.getTempDirectoryPath() );
+
+    // WHEN
+    final var result = policy.test( path );
+
+    // THEN
+    assertTrue( result );
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = { "", "/tmp/1234", "/var/log/syslog" })
+  void execute (final String path) {
+    // GIVEN
+    final var p = Paths.get( path );
+
+    // WHEN
+    final var result = policy.test( p );
+
+    // THEN
+    assertFalse( result );
+  }
+
+}
